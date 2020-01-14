f1a6b8ca727043f3fc83f2f2f2406a82b05d78b1
Kei IWASAKI <iwasaki@wacul.co.jp>
2020-01-14 20:16:28 +0900

    Queerystring parameters を試した
    ```
    $ http localhost:8080/welcome
    HTTP/1.1 200 OK
    Content-Length: 12
    Content-Type: text/plain; charset=utf-8
    Date: Tue, 14 Jan 2020 11:14:33 GMT
    
    Hello Guest
    
    $ http GET 'localhost:8080/welcome?lastname=hoge'
    HTTP/1.1 200 OK
    Content-Length: 16
    Content-Type: text/plain; charset=utf-8
    Date: Tue, 14 Jan 2020 11:14:53 GMT
    
    Hello Guest hoge
    
    $ http GET 'localhost:8080/welcome?firstname=Kei&lastname=IWASAKI'
    HTTP/1.1 200 OK
    Content-Length: 17
    Content-Type: text/plain; charset=utf-8
    Date: Tue, 14 Jan 2020 11:15:11 GMT
    
    Hello Kei IWASAKI
    
    ```

--- hello-gin/main.go
+++ hello-gin/main.go
@@ -16,6 +16,8 @@ func run() error {
 
 	router := gin.Default()
 
+	// Parameter in Path
+	// --------------------------------------------------------
 	router.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.String(http.StatusOK, "Hello %s", name)
@@ -31,6 +33,15 @@ func run() error {
 	// router.POST("/user/:name/*actoin", func(c *gin.Context) {
 	// c.FullPath() == "/user/:name/*action"
 	// })
+
+	// Queerystring parameters
+	router.GET("/welcome", func(c *gin.Context) {
+		firstname := c.DefaultQuery("firstname", "Guest")
+		lastname := c.Query("lastname")
+		// lastname := c.Request.URL.Query.GET("lastname")
+
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
 
 	return router.Run(":8080")
 }
