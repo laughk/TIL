64959ae64d6876e9e395c80f31c4cc70b51757e5
Kei IWASAKI <iwasaki@wacul.co.jp>
2020-01-14 20:35:01 +0900

    Model binding and Validation を試した

--- hello-gin/main.go
+++ hello-gin/main.go
@@ -12,6 +12,12 @@ func main() {
 	}
 }
 
+// Login ... Binding from JSON, Form
+type Login struct {
+	User     string `form:"user" json:"user" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
 func run() error {
 
 	router := gin.Default()
@@ -53,6 +59,36 @@ func run() error {
 			"message": message,
 			"nick":    nick,
 		})
+	})
+
+	// binding from JSON
+	router.POST("/loginJSON", func(c *gin.Context) {
+		var json Login
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if json.User != "manu" || json.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+
+	// binding from Form
+	router.POST("/loginForm", func(c *gin.Context) {
+		var form Login
+		if err := c.ShouldBind(&form); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if form.User != "manu" || form.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 	})
 
 	return router.Run(":8080")
